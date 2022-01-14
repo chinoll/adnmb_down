f0db03ef17735396c8811a95673bb5fabd32d06d
chinoll <chinoll@chinoll.org>
2022-01-14 22:32:07 +0800

    爬取版块

--- adnmb.js
+++ adnmb.js
@@ -11,20 +11,15 @@ let str = ''; // 最终导出到txt的字符串
 let page = 1; // 页码初始化
 
 if (!chan.match(/^[0-9]+$/)) {
-  console.error('串号输入有误，请输入纯数字的串号，如：31163008');
-  process.exit(0);
+  console.log(`正在下载: ${chan}`);
+  // console.error('串号输入有误，请输入纯数字的串号，如：31163008');
+  // process.exit(0);
 } else {
   console.log(`正在下载: No.${chan}`);
 }
 
 // 将请求的 option 转为函数, 实现翻页时 options.path 的 page 动态变化
-const options = page => {
-  return {
-    host: 'adnmb3.com',
-    port: 443,
-    path: `/t/${chan}?page=${page}`,
-  };
-};
+
 
 // 备胎岛
 // const options = (page) => {
@@ -32,9 +27,86 @@ const options = page => {
 //   port: 443,
 //   path: `/t/${chan}?page=${page}`,
 // };
-
+function wait(ms) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+};
 // 最外层的 pre_req 用于获取最大页码
-const pre_req = https.request(options(1), res => {
+const thread_req = (c) => {
+  const options = page => {
+    return {
+      host: 'adnmb3.com',
+      port: 443,
+      path: `/t/${c}?page=${page}`,
+    };
+  };
+  return https.request(options(1), async res => {
+    await wait(Math.random()*2000);
+    if (res.statusCode === 502) {
+      console.log('岛沉了，告辞');
+    } else if (res.statusCode === 200) {
+      console.log(`状态码: ${res.statusCode}, 开始获取索引`);
+      let buffArr = [];
+      let buff = null;
+      res.on('data', d => {
+        buffArr.length += 1;
+        buffArr[buffArr.length - 1] = d;
+      });
+      res.on('end', () => {
+        // 防止网站连续提供2个Buffer造成的报错
+        buffArr.length > 1 ? (buff = Buffer.concat(buffArr)) : (buff = buffArr[0]);
+        // gunzip用于解压缩gzip编码
+        zlib.gunzip(buff, async function (err, decoded) {
+          let html = decoded.toString();
+          const dom = new JSDOM(html);
+          // 判断是否只有一页
+          let page_first = dom.window.document.querySelector('.uk-pagination li:last-child').getAttribute('class');
+          if (page_first === 'uk-disabled') {
+            page = 1;
+          } else {
+            // 获取[末页]按钮的实际跳转页数
+            let page_str = dom.window.document
+              .querySelector('.uk-pagination li:last-child a')
+              .getAttribute('href')
+              .match(/.+page=(.+)/)[1];
+            page = parseInt(page_str);
+            // 如果跳转页数为2，那么获取到的不是[末页]按钮，而是下一页，
+            // 那么倒数第二个页码按钮为最大页数
+            if (page === 2) {
+              page_str = dom.window.document
+                .querySelector('.uk-pagination li:nth-last-child(2) a')
+                .getAttribute('href')
+                .match(/.+page=(.+)/)[1];
+              page = parseInt(page_str);
+            }
+          }
+          console.log('thread page: ', page);
+          // 等待所有页轮询完成
+          await threadTurner(page,options);
+          // 写入文件
+          fs.mkdir('./download', { recursive: true }, err => {
+            if (err) console.log(err);
+          });
+          fs.writeFile(`./download/No.${c}.txt`, str, function () {
+            // console.log('str: ', str);
+            console.log(`下载完成！保存在 ./download/No.${c}.txt`);
+          });
+        });
+      });
+    } else {
+      console.log('出啥情况啦？咱也不造啊|д`)', res.statusCode,c);
+    }
+  });
+};
+
+const foptions = (page) => {
+  return {
+  host:'adnmb3.com',
+  port: 443,
+  path: `/f/${encodeURIComponent(chan)}?page=${page}`,
+  }
+};
+
+const download_threads = https.request(foptions(1), res => {
   if (res.statusCode === 502) {
     console.log('岛沉了，告辞');
   } else if (res.statusCode === 200) {
@@ -52,55 +124,39 @@ const pre_req = https.request(options(1), res => {
       zlib.gunzip(buff, async function (err, decoded) {
         let html = decoded.toString();
         const dom = new JSDOM(html);
-        // 判断是否只有一页
-        let page_first = dom.window.document.querySelector('.uk-pagination li:last-child').getAttribute('class');
-        if (page_first === 'uk-disabled') {
-          page = 1;
-        } else {
-          // 获取[末页]按钮的实际跳转页数
-          let page_str = dom.window.document
-            .querySelector('.uk-pagination li:last-child a')
+
+        // 获取[末页]按钮的实际跳转页数
+        let page_str = dom.window.document
+          .querySelector('.uk-pagination li:last-child a')
+          .getAttribute('href')
+          .match(/.+page=(.+)/)[1];
+        page = parseInt(page_str);
+        // 如果跳转页数为2，那么获取到的不是[末页]按钮，而是下一页，
+        // 那么倒数第二个页码按钮为最大页数
+        if (page === 2) {
+          page_str = dom.window.document
+            .querySelector('.uk-pagination li:nth-last-child(2) a')
             .getAttribute('href')
             .match(/.+page=(.+)/)[1];
           page = parseInt(page_str);
-          // 如果跳转页数为2，那么获取到的不是[末页]按钮，而是下一页，
-          // 那么倒数第二个页码按钮为最大页数
-          if (page === 2) {
-            page_str = dom.window.document
-              .querySelector('.uk-pagination li:nth-last-child(2) a')
-              .getAttribute('href')
-              .match(/.+page=(.+)/)[1];
-            page = parseInt(page_str);
-          }
         }
         console.log('page: ', page);
-        // 等待所有页轮询完成
         await pageTurner(page);
-        // 写入文件
-        fs.mkdir('./download', { recursive: true }, err => {
-          if (err) console.log(err);
-        });
-        fs.writeFile(`./download/No.${chan}.txt`, str, function () {
-          // console.log('str: ', str);
-          console.log(`下载完成！保存在 ./download/No.${chan}.txt`);
-        });
       });
     });
   } else {
-    console.log('出啥情况啦？咱也不造啊|д`)');
+    console.log('出啥情况啦？咱也不造啊|д`)', res.statusCode);
   }
 });
 
-pre_req.on('error', error => {
+download_threads.on('error', error => {
   console.error(error);
 });
 
-pre_req.end();
-
+download_threads.end();
 // 当前页所有楼层轮询器
-const rowTurner = function (c) {
+const pageRowTurner = function (c,options) {
   return new Promise((resolve, reject) => {
-    console.log('nowPage: ', c);
     const req = https.request(options(c), res => {
       let bufferArr = [];
       let buffer = null;
@@ -112,10 +168,10 @@ const rowTurner = function (c) {
         // 防止网站连续提供2个Buffer造成的报错
         bufferArr.length > 1 ? (buffer = Buffer.concat(bufferArr)) : (buffer = bufferArr[0]);
         zlib.gunzip(buffer, function (err, decoded) {
-          // console.log(decoded.toString());// gzip解压后的html文本
+
           let html = decoded.toString();
           const dom = new JSDOM(html);
-          // console.log(dom.window.document.querySelector(".h-threads-content").textContent);
+
           let outArr = dom.window.document.querySelectorAll('.h-threads-content');
           let tidArr = dom.window.document.querySelectorAll('.h-threads-info-id'); // 获取串号
           let offset = 0; // 广告层数偏移量修正
@@ -150,11 +206,65 @@ const rowTurner = function (c) {
   });
 };
 
-// 当前串所有页轮询器
+const threadsTurner = function (c) {
+  return new Promise((resolve, reject) => {
+    console.log('nowPage: ', c);
+    const req = https.request(foptions(c), res => {
+      let bufferArr = [];
+      let buffer = null;
+      res.on('data', d => {
+        bufferArr.length += 1;
+        bufferArr[bufferArr.length - 1] = d;
+      });
+      res.on('end', () => {
+        // 防止网站连续提供2个Buffer造成的报错
+        bufferArr.length > 1 ? (buffer = Buffer.concat(bufferArr)) : (buffer = bufferArr[0]);
+        zlib.gunzip(buffer,(err, decoded) => {
+          let html = decoded.toString();
+          const dom = new JSDOM(html);
+
+          let tidArr = dom.window.document.querySelectorAll('.h-threads-item'); // 获取串号
+
+          tidArr.forEach(async (i, index) => {
+            let tid = i.getAttribute('data-threads-id');
+            console.log('downlaod thread',tid);
+            let r = thread_req(tid);
+            r.on('error', error => {
+              console.error(error);
+            });
+            r.end();
+            // await wait(1000);
+          });
+          if (err) {
+            console.log('err: ', err);
+          }
+          resolve();
+        });
+      });
+    });
+
+    req.on('error', error => {
+      console.error(error);
+    });
+
+    req.end();
+  });
+};
+
+// 调用方法；
+//当前版块的所有页轮询器
 const pageTurner = async function (page) {
+  for (let i = 1; i <= page; i++) {
+    await threadsTurner(i);
+    await wait(5000);
+
+  }
+};
+// 当前串所有页轮询器
+const threadTurner = async function (page,options) {
   for (let c = 1; c < page + 1; c++) {
     // 等待当前页所有回应（楼层）轮询完成
-    await rowTurner(c);
+    await pageRowTurner(c,options);
     str += '\n\n\n\n';
   }
 };
